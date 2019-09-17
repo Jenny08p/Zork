2389032d732814dd6700e872850281ea43698efa
Jenny08p <46802465+Jenny08p@users.noreply.github.com>
2019-09-17 19:01:42 -0400

    The game loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -18,30 +18,63 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
-            if (inputString == "QUIT")
+            Commands command = Commands.UNKNOWN;
+            while (command!= Commands.QUIT)
             {
-                Console.WriteLine("Thank you for playing.");
+                Console.Write(">");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string response; 
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        response = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        response = "This is an open field west of a white house, with a boarded front door.A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break; 
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                    response = $"You moved {command.ToString().ToLower()}";
+                    break;
+
+                    default:
+                        response = "Unknown Command";
+                        break;
+                            
+                }
+                Console.WriteLine(response);
             }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+
+
+           // string inputString = Console.ReadLine(); 
+            //inputString = inputString.ToUpper();
+            //Commands command = ToCommand(inputString.Trim().ToUpper());
+           // Console.WriteLine(command);
+           
+            //{
+             //   Console.WriteLine("Thank you for playing.");
+            //}
+            //else if (inputString == "LOOK")
+            //{
+              //  Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+           // }
+            //else
+            //{
+             //   Console.WriteLine("Unrecognized command.");
+            //}
 
         }
 
         private static Commands ToCommand(string commandString)
         {
-           if (Enum.TryParse<Commands>(commandString, true, out Commands result))
+           if (Enum.TryParse<Commands>(commandString, ignoreCase: true, out Commands result))
             {
-                return result;
+                return command;
             }
            else
             {
