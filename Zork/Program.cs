16e3b7cfef750263d3e9b4d6f61b28f918156a67
Jenny08p <46802465+Jenny08p@users.noreply.github.com>
2019-09-17 18:17:46 -0400

    finding your directin refactored

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -39,41 +39,14 @@ namespace Zork
 
         private static Commands ToCommand(string commandString)
         {
-            Commands command;
-
-            switch (commandString)
+           if (Enum.TryParse<Commands>(commandString, true, out Commands result))
             {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
-
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-
-                default:
-                    command = Commands.UNKNOWN;
-                    break;
-            };
-
-            return command;
-
+                return result;
+            }
+           else
+            {
+                return Commands.UNKNOWN;
+            }
         }
 
      }
