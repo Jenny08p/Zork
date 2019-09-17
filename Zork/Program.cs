fbdd5102d525cc4adc96ede3516f86d791842a49
Jenny08p <46802465+Jenny08p@users.noreply.github.com>
2019-09-17 19:22:51 -0400

    save

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -50,31 +50,13 @@ namespace Zork
                 Console.WriteLine(response);
             }
 
-
-           // string inputString = Console.ReadLine(); 
-            //inputString = inputString.ToUpper();
-            //Commands command = ToCommand(inputString.Trim().ToUpper());
-           // Console.WriteLine(command);
-           
-            //{
-             //   Console.WriteLine("Thank you for playing.");
-            //}
-            //else if (inputString == "LOOK")
-            //{
-              //  Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-           // }
-            //else
-            //{
-             //   Console.WriteLine("Unrecognized command.");
-            //}
-
         }
 
         private static Commands ToCommand(string commandString)
         {
            if (Enum.TryParse<Commands>(commandString, ignoreCase: true, out Commands result))
             {
-                return command;
+                return result;
             }
            else
             {
