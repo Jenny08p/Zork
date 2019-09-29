6bcb6a7ed714aea44e568f1be048e201200eea53
Jenny08p <46802465+Jenny08p@users.noreply.github.com>
2019-09-29 16:10:58 -0400

    Quick Save

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -125,7 +125,24 @@ namespace Zork
 
         private static void InitializedRoomDescription()
         {
-            Rooms[0, 0].Description = "You are on a rock-strewn trail."; // Rocky Trail
+            var roomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                roomMap[room.Name] = room;
+            }
+            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
+            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
+            roomMap["West of House"].Description = "This is an open field west of a white house, with a borded front door";
+            roomMap["Behind house"].Description = "You are behind the white house. In one corner of the house, there is a small window which is slightly ajar.";
+            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here and all the windows are barred.";
+            roomMap["Clearing"].Description = "You are in a clearing with a forest surrounding you on the west and south.";
+        }
+        
+        
+        /* Rooms[0, 0].Description = "You are on a rock-strewn trail."; // Rocky Trail
             Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred."; // South of House
             Rooms[0, 2].Description =  "You are at the top of the Great Canyon on its south wall." ;       // Canyon View
 
@@ -144,7 +161,7 @@ namespace Zork
             {new Room("Rocky Trail"),  new Room("South of House"),   new Room("Canyon View") },
             {new Room("Forest"),       new Room("West of House"),    new Room("Behind house") },
             {new Room("Dense Woods"),  new Room ("North of House"),  new Room("Clearing") }
-        };
+        }; */ 
 
         private static (int Row, int Column) Location;
 
