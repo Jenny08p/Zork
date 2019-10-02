b9038312dcd79e90b6aca0fa70140e3d83ea97e1
Jenny08p <46802465+Jenny08p@users.noreply.github.com>
2019-10-02 12:17:23 -0400

    Middle of chapter 12
    10 errors, code isn't finished

---
+++ Zork/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Zork
+{
+    public class Game
+    {
+        public World World { get; }
+    }
+
+    public static Game Load(string filename)
+    {
+        Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
+        return game;
+    }
+}
---
+++ Zork/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Zork
+{
+    public class Player
+    {
+        public World world { get; }
+
+        [JsonIgnore]
+        public Room Location { get; private set; }
+
+        [JsonIgnore]
+
+        public string LocationName
+        {
+            get
+            {
+                return Location?.Name;
+            }
+            set
+            {
+                Location = World?.RoomsByName.GetValueOrDefault(value);
+            }
+
+        }
+
+
+            public bool Move(Directions direction)
+        {
+            bool isValidMove = Location.Neighbbors.TryGetValure(direction, out Room destination);
+            if (isValidMove)
+            {
+                Location = destination;
+            }
+
+            return isValidMove;
+            }
+        }
+    }
